654c116a5488de1b7a53db097cd0ddb9a1b081b9
maxwell <maxwell@GROCK.localdomain>
2018-08-04 09:30:45 +0800

    add project

---
+++ src/main/java/com/hackzone/concurrencymc/ConcurrencyMcApplication.java
@@ -0,0 +1,12 @@
+package com.hackzone.concurrencymc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyMcApplication.class, args);
+	}
+}
